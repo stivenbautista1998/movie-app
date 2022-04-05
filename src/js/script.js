78d38de5b1899ba18ca81e602c29526f562856c7
Stiven Bautista <stivenb1994@gmail.com>
2022-04-05 18:33:24 -0500

    adding the horror, mistery and animation section

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,37 @@
-import { getMovieById, getTopMovies, getAllGenres, getMoviesByGenre } from './utils/connections.js';
-let divFirstMovie, divTrendingMovies, divRomanticMovies;
+import { getMovieById, getTopMovies, getMoviesByGenre } from './utils/connections.js';
+let divFirstMovie, divTrendingMovies, divRomanticMovies, divAnimationMovies, divHorrorMovies, divMysteryMovies;
 
 const GENRESTOSHOW = {
     romance: 10749,
     animation: 16,
-    comedy: 35,
     horror: 27,
-    Fantasy: 14
+    mystery: 9648
 }
 
 window.addEventListener("load", () => {
     divFirstMovie = document.querySelector("#js-first-movie-section");
     divTrendingMovies = document.querySelector("#js-trending-movies");
     divRomanticMovies = document.querySelector("#js-romantic-movies");
+    divAnimationMovies = document.querySelector("#js-animation-movies");
+    divHorrorMovies = document.querySelector("#js-horror-movies");
+    divMysteryMovies = document.querySelector("#js-mystery-movies");
     
     renderFirstMovie(); // first movie to show.
     renderTopMovies();
     renderRomanceMovies();
+    renderAnimationMovies();
+    renderMysteryMovies();
+    renderHorrorMovies();
 });
 
 async function renderFirstMovie() {
-    const data = await getMovieById("76341");
+    const data = await getMovieById("406759"); // 76341 naruto: 317442
     divFirstMovie.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${data.poster_path}')`;
 }
 
 function renderMovies(moviesInfo) {
     let movieList = "";
     
-    console.log(moviesInfo);
     moviesInfo.forEach(movie => { 
         if(movie?.poster_path) {
             movieList += 
@@ -57,6 +61,27 @@ async function renderRomanceMovies() {
     const romanceMovies = renderMovies(moviesInfo);
     
     divRomanticMovies.innerHTML = romanceMovies;
+}
+
+async function renderAnimationMovies() {
+    const moviesInfo = await getMoviesByGenre(GENRESTOSHOW.animation);
+    const animationMovies = await renderMovies(moviesInfo);
+
+    divAnimationMovies.innerHTML = animationMovies;
+}
+
+async function renderMysteryMovies() {
+    const moviesInfo = await getMoviesByGenre(GENRESTOSHOW.mystery);
+    const mysteryMovies = await renderMovies(moviesInfo);
+
+    divMysteryMovies.innerHTML = mysteryMovies;
+}
+
+async function renderHorrorMovies() {
+    const moviesInfo = await getMoviesByGenre(GENRESTOSHOW.horror);
+    const horrorMovies = await renderMovies(moviesInfo);
+
+    divHorrorMovies.innerHTML = horrorMovies;
 }
 
 // const genresList = await getAllGenres();
