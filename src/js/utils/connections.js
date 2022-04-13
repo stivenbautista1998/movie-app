5e0e2d52789f427dc95ea8c48ade48e8cf279e70
Stiven Bautista <stivenb1994@gmail.com>
2022-04-13 17:00:02 -0500

    reducing code from connections.js

--- src/js/utils/connections.js
+++ src/js/utils/connections.js
@@ -1,11 +1,8 @@
-const apiKey = "7518e90bd9bc2a6fa53eef1c15f77b7d";
+const API_KEY = "7518e90bd9bc2a6fa53eef1c15f77b7d";
 
-
-async function getMovieById(movieId) {
-    const endpoint = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
-    
+async function getFetchInfo(myEndPoint) {
     try {
-        let result = await fetch(endpoint);
+        let result = await fetch(myEndPoint);
         let data = await result.json();
         return data;
     } catch (err) {
@@ -13,20 +10,19 @@ async function getMovieById(movieId) {
     }
 }
 
-async function getTrending() {
-    const endpoint = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`;
+async function getMovieById(movieId) {
+    const endpoint = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+    const data = await getFetchInfo(endpoint);
+    return data;
+}
 
-    try {
-        const result = await fetch(endpoint);
-        const data = await result.json();
-        return data.results;
-    } catch (err) {
-        console.warn(err.message);
-    }
+async function getTrending() {
+    const endpoint = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
+    const data = await getFetchInfo(endpoint);
+    if(data?.results) return data.results;
 }
 
 async function getTopMovies() {
-
     try {
         const result = await getTrending();
         // const moviesSelected = result.slice(0, limit);
@@ -39,27 +35,15 @@ async function getTopMovies() {
 }
 
 async function getAllGenres() {
-    const endPoint = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
-
-    try {
-        const result = await fetch(endPoint);
-        const data = await result.json();
-        return data;
-    } catch (err) {
-        console.warn(err.message);
-    }
+    const endPoint = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    const data = await getFetchInfo(endPoint);
+    return data;
 }
 
 async function getMoviesByGenre(genreId) {
-    const endPoint = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`;
-
-    try {
-        const result = await fetch(endPoint);
-        const data = await result.json();
-        return data.results;
-    } catch (err) {
-        console.warn(err.message);
-    }
+    const endPoint = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`;
+    const data = await getFetchInfo(endPoint);
+    if(data?.results) return data.results;
 }
 
 export { getMovieById, getTopMovies, getAllGenres, getMoviesByGenre };
