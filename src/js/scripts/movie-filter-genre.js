393240095b22232eea88cf93bfddf3cc21dccbc4
Stiven Bautista <stivenb1994@gmail.com>
2022-04-27 19:42:59 -0500

    adding the serach input filter to the movie filter and tv filter page

--- src/js/scripts/movie-filter-genre.js
+++ src/js/scripts/movie-filter-genre.js
@@ -4,44 +4,108 @@ import {
     IMAGE_URL 
 } from '../utils/connections.js';
 import { registerMovie } from '../utils/observer.js';
-let rootApp, searchInput;
+let rootApp, searchInput, searchResultContainer, showAllMovieInfo;
 
 window.addEventListener("load", () => {
     rootApp = document.querySelector("#app");
     searchInput = document.querySelector("#js-search-input");
+    searchResultContainer = document.querySelector("#js-search-results");
     const genreId = getParameters("genreId");
     const genreName  = getParameters("genreName");
     showMovieByGenreSelected(genreId, genreName);
 
-    searchInput.addEventListener("keydown", searchMovie);
+    searchInput.addEventListener("keyup", searchMovie);
 });
 
 async function searchMovie(event) {
-    if(event.keyCode === 13) {      
-        let { value } = event.target; 
+    let { value } = event.target; 
+    if(event.keyCode === 13) {
         if(value !== "") {
-            const data = await queryWithWord(value, "movie");
-
-            if(data.length !== 0) {
-                const movieResults = renderMovies(data);
-
-                rootApp.innerHTML = `
-                <section class="general-section">
-                    <h2 class="first-tittle">Movie Results</h2>
-                    <div class="movie-search-items">
-                        ${movieResults}
-                    </div>
-                </section>`;
-                observingMovies();
-            } else {
-                rootApp.innerHTML = `
-                <section class="general-section">
-                    <div class="not-found-section">
-                        <img class="not-found-image" src="../assets/imgs/not-found.png" alt="not found image">
-                    </div>
-                </section>`;
-            }
+            showMoviesFilteredBySearch(value);
         }
+    } else if(value !== "") {
+        let queryResult = await queryOfInput(value, 5);
+
+        if(queryResult !== null && queryResult !== undefined) {
+            searchResultContainer.innerHTML = queryResult;
+
+            if(queryResult !== `<div class="query-message">No results found</div>`) {
+                observingMovies();
+                showAllMovieInfo = document.querySelector("#js-view-all-btn");
+                showAllMovieInfo.onclick = () => {
+                    console.log("it has been clicked!!");
+                    showMoviesFilteredBySearch(value);
+                };
+            }
+
+        } else {
+            searchResultContainer.innerHTML = "";
+        }
+    } else {
+        searchResultContainer.innerHTML = "";
+    }
+}
+
+async function queryOfInput(inputText, limite) {
+    if(inputText.length > 3) {
+        const data = await queryWithWord(inputText, "movie");    
+        if(data.length !== 0) {
+            const result = data.slice(0, limite);
+            const movieSearchList = showSearchList(result);
+            return movieSearchList;
+        } else {
+            return `<div class="query-message">No results found</div>`;
+        }
+    } else {
+        return null;
+    }
+}
+
+function showSearchList(data) {
+    let queryList = "";
+
+    data.forEach((movieInfo) => {
+        let datasetImage = `data-img-url="url('${IMAGE_URL + movieInfo.poster_path}')"`;
+
+        queryList += `
+        <a class="no-link-style" href="/src/views/movie-info.html?movieId=${movieInfo.id}">
+            <div class="query-list">
+                <div class="movie-image query-list-img" ${movieInfo.poster_path !== null ? datasetImage : ""}>
+                    ${movieInfo.poster_path !== null ? "" : `<span class="center-message white-message">No Image</span>`}
+                </div>
+                <div class="query-list-title">${movieInfo.title}</div>
+            </div>
+        </a>`;
+    });
+    return `
+        ${queryList}
+        <div id="js-view-all-btn" class="query-list-btn">View all results</div>
+    `;
+}
+
+async function showMoviesFilteredBySearch(value) {
+    const data = await queryWithWord(value, "movie");
+
+    if(data.length !== 0) {
+        const movieResults = renderMovies(data);
+
+        rootApp.innerHTML = `
+        <section class="general-section">
+            <h2 class="first-tittle">Movie Results</h2>
+            <div class="movie-search-items">
+                ${movieResults}
+            </div>
+        </section>`;
+        observingMovies();
+        searchResultContainer.innerHTML = "";
+    } else {
+        rootApp.innerHTML = `
+        <section class="general-section">
+            <div class="not-found-section">
+                <img class="not-found-image" src="../assets/imgs/not-found.png" alt="not found image">
+            </div>
+        </section>`;
+        searchResultContainer.innerHTML = "";
     }
 }
 
--- src/js/scripts/tv-filter-genre.js
+++ src/js/scripts/tv-filter-genre.js
@@ -4,44 +4,108 @@ import {
     IMAGE_URL 
 } from '../utils/connections.js';
 import { registerMovie as registerTvSerie } from '../utils/observer.js';
-let rootApp, searchInput;
+let rootApp, searchInput, searchResultContainer, showAllMovieInfo;
 
 window.addEventListener("load", () => {
     rootApp = document.querySelector("#app");
     searchInput = document.querySelector("#js-search-input");
+    searchResultContainer = document.querySelector("#js-search-results");
     const genreId = getParameters("genreId");
     const genreName  = getParameters("genreName");
     showTvSeriesByGenreSelected(genreId, genreName);
 
-    searchInput.addEventListener("keydown", searchTvSerie);
+    searchInput.addEventListener("keyup", searchTvSerie);
 });
 
 async function searchTvSerie(event) {
+    let { value } = event.target; 
     if(event.keyCode === 13) {
-        let { value } = event.target; 
         if(value !== "") {
-            const data = await queryWithWord(value, "tv");
-
-            if(data.length !== 0) {
-                const tvResults = renderSeries(data);
-
-                rootApp.innerHTML = `
-                <section class="general-section">
-                    <h2 class="first-tittle">Series Results</h2>
-                    <div class="movie-search-items">
-                        ${tvResults}
-                    </div>
-                </section>`;
-                observingTvSerie();
-            } else {
-                rootApp.innerHTML = `
-                <section class="general-section">
-                    <div class="not-found-section">
-                        <img class="not-found-image" src="../assets/imgs/not-found.png" alt="not found image">
-                    </div>
-                </section>`;
-            }
+            showTvSeriesFilteredBySearch(value);
         }
+    } else if(value !== "") {
+        let queryResult = await queryOfInput(value, 5);
+
+        if(queryResult !== null && queryResult !== undefined) {
+            searchResultContainer.innerHTML = queryResult;
+
+            if(queryResult !== `<div class="query-message">No results found</div>`) {
+                observingTvSerie();
+                showAllMovieInfo = document.querySelector("#js-view-all-btn");
+                showAllMovieInfo.onclick = () => {
+                    console.log("it has been clicked!!");
+                    showTvSeriesFilteredBySearch(value);
+                };
+            }
+
+        } else {
+            searchResultContainer.innerHTML = "";
+        }
+    } else {
+        searchResultContainer.innerHTML = "";
+    }
+}
+
+async function queryOfInput(inputText, limite) {
+    if(inputText.length > 3) {
+        const data = await queryWithWord(inputText, "tv");    
+        if(data.length !== 0) {
+            const result = data.slice(0, limite);
+            const movieSearchList = showSearchList(result);
+            return movieSearchList;
+        } else {
+            return `<div class="query-message">No results found</div>`;
+        }
+    } else {
+        return null;
+    }
+}
+
+function showSearchList(data) {
+    let queryList = "";
+
+    data.forEach((tvSerieInfo) => {
+        let datasetImage = `data-img-url="url('${IMAGE_URL + tvSerieInfo.poster_path}')"`;
+
+        queryList += `
+        <a class="no-link-style" href="/src/views/tv-info.html?tvId=${tvSerieInfo.id}">
+            <div class="query-list">
+                <div class="movie-image query-list-img" ${tvSerieInfo.poster_path !== null ? datasetImage : ""}>
+                    ${tvSerieInfo.poster_path !== null ? "" : `<span class="center-message white-message">No Image</span>`}
+                </div>
+                <div class="query-list-title">${tvSerieInfo.title}</div>
+            </div>
+        </a>`;
+    });
+    return `
+        ${queryList}
+        <div id="js-view-all-btn" class="query-list-btn">View all results</div>
+    `;
+}
+
+async function showTvSeriesFilteredBySearch(value) {
+    const data = await queryWithWord(value, "tv");
+
+    if(data.length !== 0) {
+        const tvResults = renderSeries(data);
+
+        rootApp.innerHTML = `
+        <section class="general-section">
+            <h2 class="first-tittle">Series Results</h2>
+            <div class="movie-search-items">
+                ${tvResults}
+            </div>
+        </section>`;
+        observingTvSerie();
+        searchResultContainer.innerHTML = "";
+    } else {
+        rootApp.innerHTML = `
+        <section class="general-section">
+            <div class="not-found-section">
+                <img class="not-found-image" src="../assets/imgs/not-found.png" alt="not found image">
+            </div>
+        </section>`;
+        searchResultContainer.innerHTML = "";
     }
 }
 
